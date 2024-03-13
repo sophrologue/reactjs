e05322a38fd37ed04554d3e630178c532fbba82a
qazx960 <giwon.yi339@gmail.com>
2024-03-13 09:16:19 +0900

    broken footer alignment fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <footer className="h-[80px] mb-8 bg-white  flex flex-col gap-2 items-center justify-center text-gray-500">
+    <footer className="h-[80px] mb-8 bg-white  gap-2 flex flex-col items-center justify-center text-gray-500">
       <p>N° de SIRET 832 217 657 000 17</p>
-      <p>
+      <p className="text-center">
         En aucun cas la sophrologie ne remplace un traitement ou un avis médical
       </p>
       <p> © 2023. FABRICI CATHERINE. All Rights Reserved.</p>
